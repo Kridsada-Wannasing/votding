d60d7edb10d17f5422a4102fcd8401d9454719f5
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2021-02-26 13:12:59 +0700

    create auth service

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Query } from '@nestjs/common';
+import { UserService } from '../services/user.service';
+import { PartialType } from '@nestjs/mapped-types';
+import { SignUpDTO } from 'src/auth/dto/auth.dto';
+
+export class UserQueryParams extends PartialType(SignUpDTO) {}
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    @Inject(UserService.name) private readonly userService: UserService,
+  ) {}
+
+  @Get()
+  async find(@Query() query: UserQueryParams) {
+    return this.userService.find(query);
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return this.userService.findById(id);
+  }
+}
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '../entities/user.entity';
+import { Model } from 'mongoose';
+import { UserQueryParams } from '../controllers/user.controller';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async findById(id: string): Promise<User> {
+    return this.userModel.findById(id);
+  }
+
+  //   async findOne() {
+  //     return this.userModel.findOne({})
+  //   }
+
+  async find(query: UserQueryParams): Promise<User[]> {
+    return this.userModel.find(query);
+  }
+}
